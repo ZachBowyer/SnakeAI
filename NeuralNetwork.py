cfa35eba3082f2493765543ad444388a596587f2
ZachBowyer <54867730+ZachBowyer@users.noreply.github.com>
2022-05-04 23:20:52 -0700

    Added method to load weights (Used for asexual reproduction of genetic algorithm)

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,6 +5,8 @@ import numpy as np
 
 # https://keras.io/api/models/model/#model-class
 class NeuralNetwork(tf.keras.Model):
+
+    #Constructor, sets neural network architecture
     def __init__(self):
         super().__init__()
         self.model = tf.keras.Sequential()
@@ -12,19 +14,28 @@ class NeuralNetwork(tf.keras.Model):
         self.model.add(tf.keras.layers.Dense(4, activation='relu'))
         self.model.add(tf.keras.layers.Dense(4, activation='softmax'))
     
+    # Returns weights/biases as list of numpy arrays
     def getWeights(self):
-        for layer in self.model.layers:
-            weights = layer.get_weights()
-            print(np.shape(weights))
-            print(weights)
+        return self.model.get_weights()
 
+    # Given an input, run it through network and return output
     def call(self, inputs):
         #ans = self.model.predict(inputs)
         ans = self.model(inputs)
         return ans
+
+    #Used in creating new instances (Load weights from parent)
+    def loadWeights(self, weights):
+        self.model.set_weights(weights)
+    
+    #Used for mutating child networks (Slightly change weights and biases at random)
+    def mutateWeights(self):
+        x=1
 
 #vars = tf.random.uniform(shape=(10, 20))
 vars = np.array([[1, 2, 3, 4]])
 print(np.shape(vars))
 model = NeuralNetwork()
 print(model.call(vars))
+print(model.getWeights())
+model.loadWeights(model.getWeights())
