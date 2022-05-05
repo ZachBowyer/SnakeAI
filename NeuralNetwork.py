ff8d7cb75283f6f91c9bf1cdd7200baf25a39560
ZachBowyer <54867730+ZachBowyer@users.noreply.github.com>
2022-05-05 00:23:01 -0700

    A little confused on how to get shape to match for updating weights

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,9 +9,10 @@ class NeuralNetwork(tf.keras.Model):
     #Constructor, sets neural network architecture
     def __init__(self):
         super().__init__()
+        bias_initializer = tf.keras.initializers.HeNormal()
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.Dense(8, input_shape=(4,)))
-        self.model.add(tf.keras.layers.Dense(4, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(8, input_shape=(4,), bias_initializer=bias_initializer))
+        self.model.add(tf.keras.layers.Dense(4, activation='relu', bias_initializer=bias_initializer))
         self.model.add(tf.keras.layers.Dense(4, activation='softmax'))
     
     # Returns weights/biases as list of numpy arrays
@@ -30,12 +31,30 @@ class NeuralNetwork(tf.keras.Model):
     
     #Used for mutating child networks (Slightly change weights and biases at random)
     def mutateWeights(self):
-        x=1
+        for layer in self.model.layers:
+            print(layer.get_weights())
+            #newArr = np.array(layer.get_weights())
+            #print("new", newArr)
+            #layer.set_weights(layer.get_weights())
+            #newWeights = np.array(layer.get_weights())
+            #print("newW", newWeights)
+            #for weightArray in layer.get_weights():
+            #    print(weightArray)
+                #layer.set_weights(weightArray)
+            #print(layer.get_weights())
+        #for layerWeights in self.weights:   #Each layer
+        #    for tensors in layerWeights:      #Is a list of lists
+        #        print(tensors)
+        #        values = tf.keras.backend.get_value(tensors)
+        #        if values is not list: 
+        #            tensors[0] = 7
+        #            values = [values]
+        #        if values is list:
+        #            x=1
+        #        print(values)
 
 #vars = tf.random.uniform(shape=(10, 20))
 vars = np.array([[1, 2, 3, 4]])
-print(np.shape(vars))
 model = NeuralNetwork()
-print(model.call(vars))
-print(model.getWeights())
-model.loadWeights(model.getWeights())
+model.mutateWeights()
+#print(model.call(vars))
